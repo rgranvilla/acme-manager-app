08499ef1e40713bdff3661f83725ca53407135d6
Ricardo Granvilla <rgranvilla@gmail.com>
2022-08-23 17:00:46 -0300

    feat: configure eslint and prettier

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 :root {
@@ -62,4 +62,4 @@ button {
   opacity: 0.6;
   cursor: not-allowed;
 }
-`
+`;
