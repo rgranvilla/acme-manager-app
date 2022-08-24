e2bc9680f73a259a02beea4874abe6c91ee1ffc4
Ricardo Granvilla <rgranvilla@gmail.com>
2022-08-24 19:10:57 -0300

    feat: add routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Home } from "./pages/Home";
 import { ListPatient } from "./pages/ListPatient";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
-  const temporaryDevSelector = false;
   return (
-    <>
-      {temporaryDevSelector ? <Home /> : <ListPatient />}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<ListPatient />} />
+      </Routes>
 
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 }
