78fbe2d25fa649564b1e6eb1d7f34dd5ef0fb3aa
Ricardo Granvilla <rgranvilla@gmail.com>
2022-08-24 02:03:23 -0300

    feat: logomark component created

---
+++ src/components/atoms/Logomark/logomark.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: 0;
+
+  transition: background-color 0.2s;
+
+  img {
+    margin: 1.25rem;
+    width: 4.375rem;
+    object-fit: cover;
+  }
+`;
