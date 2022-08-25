c67cdc0bd5c8669f345fef5f7528039f6fb36495
Ricardo Granvilla <rgranvilla@gmail.com>
2022-08-25 06:43:21 -0300

    feat: table component modified

--- src/components/organisms/Table/index.tsx
+++ src/components/organisms/Table/index.tsx
@@ -1,8 +1,17 @@
 import { MdKeyboardArrowRight } from "react-icons/md";
+
+import { useNavigate } from "react-router-dom";
+
 import { TABLE, TR, TH, THEAD, TD } from "./table.styles";
 import { TablePropsDTO } from "./tableDTO";
 
 export function Table({ data, header }: TablePropsDTO) {
+  const navigate = useNavigate();
+
+  const handleSelectPatient = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <TABLE>
       <THEAD>
@@ -25,31 +34,39 @@ export function Table({ data, header }: TablePropsDTO) {
       </THEAD>
       <tbody>
         {data.map(
-          ({ id, name, bornDate, document_id, genre, address, status }) => (
-            <TR key={id} isInactivated={!status}>
-              <TD vert_Align="middle">
-                <MdKeyboardArrowRight size={24} />
-              </TD>
-              <TD vert_Align="middle" hor_Align="left" isBolded>
-                {name}
-              </TD>
-              <TD vert_Align="middle" hor_Align="center">
-                {bornDate}
-              </TD>
-              <TD vert_Align="middle" hor_Align="center">
-                {document_id}
-              </TD>
-              <TD vert_Align="middle" hor_Align="center">
-                {genre}
-              </TD>
-              <TD vert_Align="middle" hor_Align="left">
-                {address}
-              </TD>
-              <TD vert_Align="middle" hor_Align="center">
-                {status ? "Ativo" : "Inativo"}
-              </TD>
-            </TR>
-          ),
+          ({ id, name, bornDate, document_id, genre, address, status }) => {
+            const path = `/profile/edit/${id}`;
+            return (
+              <TR key={id} isInactivated={!status}>
+                <TD vert_Align="middle">
+                  <button
+                    type="button"
+                    onClick={() => handleSelectPatient(path)}
+                  >
+                    <MdKeyboardArrowRight size={24} />
+                  </button>
+                </TD>
+                <TD vert_Align="middle" hor_Align="left" isBolded>
+                  {name}
+                </TD>
+                <TD vert_Align="middle" hor_Align="center">
+                  {bornDate}
+                </TD>
+                <TD vert_Align="middle" hor_Align="center">
+                  {document_id}
+                </TD>
+                <TD vert_Align="middle" hor_Align="center">
+                  {genre}
+                </TD>
+                <TD vert_Align="middle" hor_Align="left">
+                  {address}
+                </TD>
+                <TD vert_Align="middle" hor_Align="center">
+                  {status ? "Ativo" : "Inativo"}
+                </TD>
+              </TR>
+            );
+          },
         )}
       </tbody>
     </TABLE>
