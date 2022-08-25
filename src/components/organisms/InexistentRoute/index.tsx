7fb31b3749219fd28b38c822052d6c5400cc4701
Ricardo Granvilla <rgranvilla@gmail.com>
2022-08-25 06:41:32 -0300

    feat: inexistent page component modified

--- src/components/organisms/InexistentRoute/index.tsx
+++ src/components/organisms/InexistentRoute/index.tsx
@@ -1,17 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
 import erro404 from "../../../assets/erro_404.jpg";
 import arrow_left from "../../../assets/arrow_left.svg";
 
-import { IconButton } from "../../atoms/IconButton";
 import { Container } from "./inexistentRoute.styles";
+import { Button } from "../../atoms/Button";
 
 export function InexistentRoute() {
+  const navigate = useNavigate();
   return (
     <Container>
       <img src={erro404} alt="Página não encontrada" />
       <h1>Página Inexistente</h1>
       <p>O endereço web fornecido não existe em nossa aplicação.</p>
       <p>Clique no botão abaixo para voltar a página inicial.</p>
-      <IconButton icon={arrow_left} title="Início" leftIcon link="/" />
+      <Button
+        icon={arrow_left}
+        label="Início"
+        leftIcon
+        callback={() => navigate("/")}
+      />
     </Container>
   );
 }
